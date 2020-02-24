8e8e1fadf08bf7d07fc476d20f1678467de827ea
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2020-02-24 15:11:47 +1300

    enable onMapChange on android since subscribing to events on android is more optimized

--- javascript/components/MapView.js
+++ javascript/components/MapView.js
@@ -714,7 +714,7 @@ class MapView extends NativeBridgeComponent(React.Component) {
       ref: nativeRef => this._setNativeRef(nativeRef),
       onPress: this._onPress,
       onLongPress: this._onLongPress,
-      // onMapChange: this._onChange,
+      onMapChange: isAndroid() ? this._onChange : undefined,
       onAndroidCallback: isAndroid() ? this._onAndroidCallback : undefined,
     };
 
